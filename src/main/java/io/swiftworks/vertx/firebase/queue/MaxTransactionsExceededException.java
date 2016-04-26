8f2b91df311a7cf41fdc195c3d32e1f8e4347054
Justin Bailey <meticoeus@gmail.com>
2016-04-26 10:08:11 -0400

    refactor: Making MaxTransaction exception constructors public

--- src/main/java/io/swiftworks/vertx/firebase/queue/MaxTransactionsExceededException.java
+++ src/main/java/io/swiftworks/vertx/firebase/queue/MaxTransactionsExceededException.java
@@ -5,23 +5,23 @@ public class MaxTransactionsExceededException  extends Exception {
 
     public static final String DEFAULT_MESSAGE = "Transaction errored too many times. No longer retrying!";
 
-    MaxTransactionsExceededException() {
+    public MaxTransactionsExceededException() {
         super(DEFAULT_MESSAGE);
     }
 
-    MaxTransactionsExceededException(String message) {
+    public MaxTransactionsExceededException(String message) {
         super(message);
     }
 
-    MaxTransactionsExceededException(String message, Throwable cause) {
+    public MaxTransactionsExceededException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    MaxTransactionsExceededException(Throwable cause) {
+    public MaxTransactionsExceededException(Throwable cause) {
         super(cause);
     }
 
-    MaxTransactionsExceededException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public MaxTransactionsExceededException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
